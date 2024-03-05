7aef13e56ee46db480162f22da71d2523cd711d8
danzigirl <140118979+danzigirl@users.noreply.github.com>
2024-03-05 17:41:07 -0300

    alice

--- views.py
+++ views.py
@@ -1,7 +1,26 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, jsonify, redirect, url_for
 
 views = Blueprint(__name__, "views")
 
 @views.route("/")
 def home():
-    return render_template("index.html", name="Tim")
+    return render_template("index.html", user="Tim")
+
+@views.route("/profile")
+def profile():
+    args = request.args
+    user = args.get('user')
+    return render_template("index.html", user=user)
+
+@views.route("/json")
+def get_json():
+    return jsonify({'user':'tim', 'coolness':10})
+
+@views.route("/data")
+def get_data():
+    data = request.json
+    return jsonify(data)
+
+@views.route("/go-to-home")
+def go_to_home():
+    return redirect(url_for("views.get_json"))
