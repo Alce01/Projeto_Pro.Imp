a378a21f226b5e0355fdbcaae733c6ef21b39e25
Alice <alicegv2005@gmail.com>
2024-03-18 22:48:57 -0300

    templates

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, redirect, url_for
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return "<p>Login</p>"
+    return render_template("login.html", user="Usuário", password="Senha")
 
 @auth.route('/logout')
 def logout():
-    return "<p>Logout</p>"
+    redirect(url_for("home.get_json"))
 
 @auth.route('/sign-up')
 def sign_up():
-    return "<p>Sign Up</p>"
+    return render_template("sign_up.html")
