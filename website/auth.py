b485345d1df2628702919ddd4f24402397598e0b
Alice <alicegv2005@gmail.com>
2024-03-19 10:32:49 -0300

    erro msg

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 
 auth = Blueprint('auth', __name__)
 
@@ -16,18 +16,21 @@ def logout():
 def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
-        firstName = request.form.get('fisrtName')
+        firstName = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        if len(email) > 4:
+        if len(email) < 5:
+            flash('O email deve ter mais de 5 caracteres', category='error')
             pass
         elif len(firstName) < 2:
-            pass
+            flash('Seu nome deve ter mais de 2 caracteres', category='error')
         elif password1 != password2:
-            pass
+            flash('A senha não é a mesma', category='error')
+        elif len(password1) != 12:
+            flash('A senha deve ter 12 números', category='error')
+
         else:
-            # add user to database
-            pass
+            flash('Conta criada!', category='sucess')
 
     return render_template("sign_up.html")
