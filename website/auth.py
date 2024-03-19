953bc6088b2e30a6116bdda4b5a4c4b92e708d35
Alice <alicegv2005@gmail.com>
2024-03-19 00:00:21 -0300

    http

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,17 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
+    data = request.form
+    print(data)
     return render_template("login.html", boolean=True, user="Usuário", password="Senha")
 
 @auth.route('/logout')
 def logout():
     redirect(url_for("home.get_json"))
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
     return render_template("sign_up.html")
